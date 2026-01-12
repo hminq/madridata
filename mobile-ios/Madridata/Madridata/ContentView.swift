db71db46f8477e2b117502fc94786aa63f881fd5
hminh <hminh250104@gmail.com>
2026-01-12 23:50:15 +0700

    init projects

---
+++ mobile-ios/Madridata/Madridata/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Madridata
+//
+//  Created by Thanh Hung Minh on 12/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mobile-ios/Madridata/Madridata/MadridataApp.swift
@@ -0,0 +1,17 @@
+//
+//  MadridataApp.swift
+//  Madridata
+//
+//  Created by Thanh Hung Minh on 12/1/26.
+//
+
+import SwiftUI
+
+@main
+struct MadridataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
