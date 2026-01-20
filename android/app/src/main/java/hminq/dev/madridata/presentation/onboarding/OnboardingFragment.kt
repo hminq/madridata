deedae86aa772b42fc967213c9a8e274fd8d0c73
hminh <hminh250104@gmail.com>
2026-01-21 04:03:27 +0700

    feat: add onboarding screens and main activity

---
+++ android/app/src/main/java/hminq/dev/madridata/presentation/onboarding/OnboardingFragment.kt
@@ -0,0 +1,38 @@
+package hminq.dev.madridata.presentation.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import hminq.dev.madridata.R
+import hminq.dev.madridata.databinding.FragmentOnboardingBinding
+
+
+class OnboardingFragment : Fragment() {
+    private var _binding: FragmentOnboardingBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentOnboardingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListeners()
+    }
+
+    private fun initListeners() {
+        with(binding) {
+            btnStart.setOnClickListener {
+                findNavController().navigate(R.id.action_onboardingFragment_to_favPlayerFragment)
+            }
+        }
+    }
+}
