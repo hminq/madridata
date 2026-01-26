2ecd6f37fcaea2bc59052dcbded667fdc1a951fd
hminh <hminh250104@gmail.com>
2026-01-27 03:13:36 +0700

    feat: enable hilt in main activity

--- android/app/src/main/java/hminq/dev/madridata/presentation/MainActivity.kt
+++ android/app/src/main/java/hminq/dev/madridata/presentation/MainActivity.kt
@@ -3,8 +3,10 @@ package hminq.dev.madridata.presentation
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 import hminq.dev.madridata.R
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
