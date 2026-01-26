d67fde55051f9bd84e7ae915d54ba094bc640ef3
hminh <hminh250104@gmail.com>
2026-01-27 03:12:54 +0700

    feat: implement fav player screen

--- android/app/src/main/java/hminq/dev/madridata/presentation/onboarding/FavPlayerFragment.kt
+++ android/app/src/main/java/hminq/dev/madridata/presentation/onboarding/FavPlayerFragment.kt
@@ -1,60 +1,87 @@
 package hminq.dev.madridata.presentation.onboarding
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import hminq.dev.madridata.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.GridLayoutManager
+import dagger.hilt.android.AndroidEntryPoint
+import hminq.dev.madridata.databinding.FragmentFavPlayerBinding
+import kotlinx.coroutines.launch
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FavPlayerFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+@AndroidEntryPoint
 class FavPlayerFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private var _binding: FragmentFavPlayerBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: FavPlayerViewModel by viewModels()
+    private lateinit var adapter: FavPlayerAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFavPlayerBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+        observeUiState()
+    }
+
+    private fun setupRecyclerView() {
+        adapter = FavPlayerAdapter { player ->
+            viewModel.onPlayerClicked(player)
+        }
+
+        val spanCount = 2
+        val middleSpacing = (12 * resources.displayMetrics.density).toInt()
+        val bottomSpacing = (16 * resources.displayMetrics.density).toInt()
+
+        binding.rvFavPlayer.apply {
+            layoutManager = GridLayoutManager(requireContext(), spanCount)
+            adapter = this@FavPlayerFragment.adapter
+            addItemDecoration(
+                FavPlayerItemDecoration(
+                    spanCount = spanCount,
+                    middleSpacing = middleSpacing,
+                    bottomSpacing = bottomSpacing
+                )
+            )
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fav_player, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FavPlayerFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FavPlayerFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun observeUiState() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState.collect { state ->
+                    when (state) {
+                        is FavPlayerUiState.Loading -> {
+                            adapter.setLoading(true)
+                        }
+                        is FavPlayerUiState.Success -> {
+                            adapter.submitPlayers(state.players)
+                        }
+                        is FavPlayerUiState.Error -> {
+                            // Could show error message here
+                        }
+                    }
                 }
             }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
